0a6876d481b9be60294fb76712b749924a31d067
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-02 17:29:49 -0500

    Speech Recognition finished

--- Azure/AzureSpeechToTextTest/AzureSpeechToTextTest/Program.cs
+++ Azure/AzureSpeechToTextTest/AzureSpeechToTextTest/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
 
 namespace AzureSpeechToTextTest
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task FromFile(SpeechConfig speechConfig)
         {
-            Console.WriteLine("Hello World!");
+            //using var audioConfig = AudioConfig.FromWavFileInput(@"C:\Users\juanm\Documents\GitHub\Youngermaster\Speech-Recognition-tutorials\Azure\AzureSpeechToTextTest\AzureSpeechToTextTest\rpiRecording.wav");
+            using var audioConfig = AudioConfig.FromWavFileInput(@"C:\Users\juanm\Documents\GitHub\Youngermaster\Speech-Recognition-tutorials\Azure\AzureSpeechToTextTest\AzureSpeechToTextTest\ILikePizza.wav");
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task FromMic(SpeechConfig speechConfig)
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task Main(string[] args)
+        {
+            var region = "";
+            var apiKey = "";
+            var speechConfig = SpeechConfig.FromSubscription(apiKey, region);
+            await FromFile(speechConfig);
+            await FromMic(speechConfig);
+            
         }
     }
 }
